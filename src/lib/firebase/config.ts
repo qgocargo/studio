e2c2dc6ec8a938d3d25e401d022075d855a38327
akif bade <kiffbade@gmail.com>
2025-09-09 07:59:41 +0000

    // --- Firebase Configuration ---         const firebaseConfig = {

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyAAulR2nJQm-4QtNyEqKTnnDPw-iKW92Mc",
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "my-job-file-system.firebaseapp.com",
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "my-job-file-system",
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "my-job-file-system.appspot.com",
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "145307873304",
-    appId: process.env.NEXT_PUBLIC_FIREbase_APP_ID || "1:145307873304:web:d661ea6ec118801b4a136d",
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-8EHX5K7YHL"
+    apiKey: "AIzaSyAAulR2nJQm-4QtNyEqKTnnDPw-iKW92Mc",
+    authDomain: "my-job-file-system.firebaseapp.com",
+    projectId: "my-job-file-system",
+    storageBucket: "my-job-file-system.appspot.com",
+    messagingSenderId: "145307873304",
+    appId: "1:145307873304:web:d661ea6ec118801b4a136d",
+    measurementId: "G-8EHX5K7YHL"
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
